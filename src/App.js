478dce6ee006c637c021ea353d6c2c4ffff462a1
Misha <mishagoldenberg@gmail.com>
2023-02-19 02:07:18 -0800

    fixed chatroom buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { useRef, useState } from 'react';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
 
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/auth';
-import 'firebase/compat/analytics';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/auth";
+import "firebase/compat/analytics";
 import { getAuth, onAuthStateChanged } from "firebase/compat/auth";
-import firebaseConfig from './firebaseConfig.json'
+import firebaseConfig from "./firebaseConfig.json";
 
-
-
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp(firebaseConfig);
 
@@ -19,69 +17,93 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 // const analytics = firebase.analytics();
 
-
 function App() {
   const [user] = useAuthState(auth);
-  const [currentPage, setCurrentPage] = useState('allGroups');
+  const [currentPage, setCurrentPage] = useState("allGroups");
 
-    function handleChatRoom() {
-        setCurrentPage('chatRoom');
-    }
+  function handleChatRoom() {
+    setCurrentPage("chatRoom");
+  }
 
-    function handleAllGroups() {
-        setCurrentPage('allGroups');
-    }
+  function handleAllGroups() {
+    setCurrentPage("allGroups");
+  }
 
+  console.log(currentPage);
   return (
     <div className="App">
       <header>
         <h1>Lost in Translation</h1>
         <SignOut />
+        <Back handleAllGroups={() => setCurrentPage("allGroups")} />
       </header>
 
       <section>
-         {/* {user ? <AllGroups /> : <SignIn />} */}
-        {user ? currentPage === 'allGroups' ? <AllGroups handleChatRoom={handleChatRoom} /> :<ChatRoom /> : <SignIn />}
-       </section>
+        {/* {user ? <AllGroups /> : <SignIn />} */}
+        {user ? (
+          currentPage == "allGroups" ? (
+            <AllGroups handleChatRoom={() => setCurrentPage("chatRoom")} />
+          ) : (
+            <ChatRoom />
+          )
+        ) : (
+          <SignIn />
+        )}
+      </section>
     </div>
   );
 
-    //   <section>
-    //     {user ? <AllGroups /> : <SignIn />}
-    //   </section>
+  //   <section>
+  //     {user ? <AllGroups /> : <SignIn />}
+  //   </section>
 }
 
 function SignIn() {
-
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
-  }
+  };
 
   return (
     <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      <p>
+        Do not violate the community guidelines or you will be banned for life!
+      </p>
     </>
-  )
-
+  );
 }
 
 function SignOut() {
-  return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-  )
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
 }
 
-function ChatRoom(){
+function Back({ handleAllGroups }) {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => handleAllGroups()}>
+        back
+      </button>
+    )
+  );
+}
+
+function ChatRoom() {
   const dummy = useRef();
-  const messagesRef = firestore.collection('chats');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const messagesRef = firestore.collection("chats");
+  const query = messagesRef.orderBy("createdAt").limit(25);
 
-  const [messages] = useCollectionData(query, { idField: 'id' });
+  const [messages] = useCollectionData(query, { idField: "id" });
 
-  const [formValue, setFormValue] = useState('');
-
+  const [formValue, setFormValue] = useState("");
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -92,67 +114,82 @@ function ChatRoom(){
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
-    })
+      photoURL,
+    });
 
-    setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-  return (<>
-    <main>
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
 
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <span ref={dummy}></span>
+      </main>
 
-      <span ref={dummy}></span>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="say something nice"
+        />
 
-    </main>
-
-    <form onSubmit={sendMessage}>
-
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-
-      <button class="sendbtn" type="submit" disabled={!formValue}>➤➤➤</button>
-
-    </form>
-  </>)
+        <button class="sendbtn" type="submit" disabled={!formValue}>
+          ➤➤➤
+        </button>
+      </form>
+    </>
+  );
 }
 
-function AllGroups(handleChatRoom){
-
-    return (<>
-        <form>
-          <button class="rooms" onClick={handleChatRoom}>Chatroom 1</button>
-          <button class="rooms" onClick={handleChatRoom}>Chatroom 2</button>
-          <button class="rooms">Chatroom 3</button>
-          <button class="rooms">Chatroom 4</button>
-          <button class="rooms">Chatroom 5</button>
-        </form>
-      </>);
+function AllGroups({ handleChatRoom }) {
+  return (
+    <>
+      <form>
+        <button class="rooms" onClick={() => handleChatRoom()}>
+          Chatroom 1
+        </button>
+        <button class="rooms" onClick={() => handleChatRoom()}>
+          Chatroom 2
+        </button>
+        <button class="rooms">Chatroom 3</button>
+        <button class="rooms">Chatroom 4</button>
+        <button class="rooms">Chatroom 5</button>
+      </form>
+    </>
+  );
 }
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img class="profileImg" src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <img src={require('./test.jpg')}/>
-    </div>
-  </>)
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          class="profileImg"
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+        />
+        <img src={require("./test.jpg")} />
+      </div>
+    </>
+  );
 }
 
-
 function ListChats() {
-  const chatsRef = firestore.collection('chats');
-  const query = chatsRef.orderBy('createdAt').limit(25);
+  const chatsRef = firestore.collection("chats");
+  const query = chatsRef.orderBy("createdAt").limit(25);
 
-  const [messages] = useCollectionData(query, { idField: 'id' });
+  const [messages] = useCollectionData(query, { idField: "id" });
 
-  const [formValue, setFormValue] = useState('');
-
+  const [formValue, setFormValue] = useState("");
 
   const createChat = async (e) => {
     e.preventDefault();
@@ -163,29 +200,32 @@ function ListChats() {
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
-    })
+      photoURL,
+    });
 
-    setFormValue('');
-  }
+    setFormValue("");
+  };
 
-  return (<>
-    <main>
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </main>
 
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      <form onSubmit={createChat}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="say something nice"
+        />
 
-    </main>
-
-    <form onSubmit={createChat}>
-
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-
-      <button type="submit" disabled={!formValue}>🕊️</button>
-
-    </form>
-  </>)
+        <button type="submit" disabled={!formValue}>
+          🕊️
+        </button>
+      </form>
+    </>
+  );
 }
-
-
 
 export default App;
